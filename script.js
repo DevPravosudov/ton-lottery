558414ff2e10ae76085ab335efacc41be5ae99e2
Pravosudov <dev.pravosudov@gmail.com>
2024-05-09 18:32:14 +0400

    change script with ton connection logic

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-// Инициализация экземпляра TonConnectUI для подключения и взаимодействия с TON blockchain.
-const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
-    manifestUrl: 'https://devpravosudov.github.io/ton-lottery/tonconnect-manifest.json',
-    buttonRootId: 'ton-connect'
-});
+// Добавление обработчика события 'DOMContentLoaded' для выполнения кода после полной загрузки DOM.
+document.addEventListener('DOMContentLoaded', function() {
+    // Инициализация экземпляра TonConnectUI для подключения и взаимодействия с TON blockchain.
+    const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+        manifestUrl: 'https://devpravosudov.github.io/ton-lottery/tonconnect-manifest.json',
+        buttonRootId: 'ton-connect'
+    });
 
-// Подписка на изменения статуса подключения
-tonConnectUI.onStatusChange(wallet => {
-    // Проверка, подключен ли кошелек и есть ли информация о балансе
-    if (wallet && wallet.balance) {
-        // Обновление баланса на веб-странице
-        document.getElementById('wallet-balance').innerText = `Баланс: ${wallet.balance}`;
-    } else {
-        // Если кошелек отключен или информация о балансе недоступна, отобразить сообщение
-        document.getElementById('wallet-balance').innerText = 'Баланс: Н/Д';
-    }
+    // Подписка на изменения статуса подключения
+    tonConnectUI.onStatusChange(wallet => {
+        // Проверка, подключен ли кошелек и есть ли информация о балансе
+        if (wallet && wallet.balance) {
+            // Обновление баланса на веб-странице
+            document.getElementById('wallet-balance').innerText = `Баланс: ${wallet.balance}`;
+        } else {
+            // Если кошелек отключен или информация о балансе недоступна, отобразить сообщение
+            document.getElementById('wallet-balance').innerText = 'Баланс: Н/Д';
+        }
+    });
 });
-
