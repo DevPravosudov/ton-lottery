996d20ac49e5e92e1e8d766eac8dd15fac8fb9b1
Pravosudov <dev.pravosudov@gmail.com>
2024-05-09 22:16:13 +0400

    add script for fetch user balance

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+const { TonClient } = require("ton-client-js");
+
+// Создание экземпляра клиента TON
+const client = new TonClient({
+    network: {
+        endpoints: ['https://net.ton.dev'] // URL к одному из доступных API блокчейна TON
+    }
+});
+
+// Функция для получения баланса
+async function getWalletBalance(address) {
+    try {
+        const { result } = await client.net.query_collection({
+            collection: 'accounts',
+            filter: { id: { eq: address } },
+            result: 'balance'
+        });
+
+        if (result.length > 0) {
+            const balance = result[0].balance;
+            return balance;
+        }
+        return null;
+    } catch (error) {
+        console.error("Ошибка:", error);
+        return null;
+    }
+}
+
 // Добавление обработчика события 'DOMContentLoaded' для выполнения кода после полной загрузки DOM.
 document.addEventListener('DOMContentLoaded', function() {
     console.log('DOM полностью загружен и разобран');
@@ -10,13 +39,16 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log('tonConnectUI создан');
 
     // Подписка на изменения статуса подключения
-    tonConnectUI.onStatusChange(wallet => {
+    tonConnectUI.onStatusChange(async wallet => {
         console.log('tonConnectUI onStatusChange called');
         console.log(`tonConnectUI wallet: ${JSON.stringify(wallet, null, 2)}`);
         console.log(`tonConnectUI wallet.account.pyblicKey: ${wallet.account.publicKey}`);
 
+        const balance = await getWalletBalance(wallet.account.address);
+        console.log(`balance from TON: ${balance}`);
+
         // Проверка, подключен ли кошелек и есть ли информация о балансе
-        if (wallet && wallet.balance) {
+        if (wallet && wallet.account.publicKey) {
             console.log('tonConnectUI onStatusChange wallet exist and wallet.balance');
             // Обновление баланса на веб-странице
             document.getElementById('wallet-balance').innerText = `Баланс: ${wallet.balance}`;
