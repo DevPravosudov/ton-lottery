b3db4859a3f554584a0d28f36e9bc1caa19736dc
Pravosudov <dev.pravosudov@gmail.com>
2024-05-09 18:57:23 +0400

    add logs for console in script for debugging

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 // Добавление обработчика события 'DOMContentLoaded' для выполнения кода после полной загрузки DOM.
 document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM полностью загружен и разобран');
     // Инициализация экземпляра TonConnectUI для подключения и взаимодействия с TON blockchain.
     const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
         manifestUrl: 'https://devpravosudov.github.io/ton-lottery/tonconnect-manifest.json',
         buttonRootId: 'ton-connect'
     });
 
+    console.log('tonConnectUI создан');
+
     // Подписка на изменения статуса подключения
     tonConnectUI.onStatusChange(wallet => {
+        console.log('tonConnectUI onStatusChange called');
+
         // Проверка, подключен ли кошелек и есть ли информация о балансе
         if (wallet && wallet.balance) {
+            console.log('tonConnectUI onStatusChange wallet exist and wallet.balance');
             // Обновление баланса на веб-странице
             document.getElementById('wallet-balance').innerText = `Баланс: ${wallet.balance}`;
         } else {
+            console.log('tonConnectUI onStatusChange else closure called');
             // Если кошелек отключен или информация о балансе недоступна, отобразить сообщение
-            document.getElementById('wallet-balance').innerText = 'Баланс: Н/Д';
+            document.getElementById('wallet-balance').innerText = 'Баланс: Н/Д ???';
         }
     });
 });
